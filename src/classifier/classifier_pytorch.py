e1dc51fa8589a66fa4799df420859a7a68545c41
Alex <alexandre.meyer@univ-lyon1.fr>
2018-10-09 15:17:23 +0200

    pytorch

--- src/classifier/classifier_pytorch.py
+++ src/classifier/classifier_pytorch.py
@@ -82,15 +82,12 @@ def main():
 
     ############ TRAINNING
     for epoch in range(100):  # loop over the dataset multiple times
-        inputs, labels = next_batch(128)
-        inputs = torch.from_numpy(inputs)
-        labels = torch.from_numpy(labels).long()
         running_loss = 0.0
-        #print(str(inputs.shape))
-        #print(str(labels.shape))
-        for i in range(100):         # mini batch
-            # get the inputs
-            
+        for i in range(100):         # mini batch         
+            inputs, labels = next_batch(128)
+            inputs = torch.from_numpy(inputs)
+            labels = torch.from_numpy(labels).long()
+
             # zero the parameter gradients
             optimizer.zero_grad()
     
@@ -104,7 +101,7 @@ def main():
             running_loss += loss.item()
             if i % 100 == 99:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+                        (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
     print('Finished Training')
