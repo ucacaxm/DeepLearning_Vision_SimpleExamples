9976737c974ffdad318b021d5983c2d3e8bd62c1
Alex <alexandre.meyer@univ-lyon1.fr>
2018-10-12 17:51:52 +0200

    shape5

--- src/classifier/classifier_image_CNN_pytorch_shapes5.py
+++ src/classifier/classifier_image_CNN_pytorch_shapes5.py
@@ -57,12 +57,12 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d( 3, 6, 5)
+        self.conv1 = nn.Conv2d( 1, 6, 5)
         self.pool = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d( 6, 16, 5)
         self.fc1 = nn.Linear( 16*5*5, 128)
         self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(84, 10)         # 5 classes
+        self.fc3 = nn.Linear(84, 5)         # 5 classes
 
     def forward(self, x):
         x = self.pool( F.relu(self.conv1(x)))       # (c,w,h)  1x32x32 => 29*29*10 => 15*15*10
@@ -104,22 +104,6 @@ if __name__ == "__main__":
     mydata = ImageFolder(root="../../data/shapes5", transform=TRANSFORM_IMG)
     loader = DataLoader(mydata, batch_size=16, shuffle=True, num_workers=2)
 
-
-
-
-    transform = transforms.Compose(
-                [transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    mydata = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    loader = torch.utils.data.DataLoader(mydata, batch_size=4,
-                                            shuffle=True, num_workers=2)
-
-    testdata = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testdata, batch_size=4,
-                                         shuffle=False, num_workers=2)
 
 
     print(mydata)
@@ -180,4 +164,17 @@ if __name__ == "__main__":
     _, predicted = torch.max(outputs, 1)
     print('Predicted: ', ' '.join('%5s' % predicted[j] for j in range(4)))		  
     
+    ############# EVAL
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
 
