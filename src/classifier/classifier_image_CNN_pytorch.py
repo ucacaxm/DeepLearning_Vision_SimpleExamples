16e3b1766fc8475bcfe94b40f11d6ebe7e36578d
Alex <alexandre.meyer@univ-lyon1.fr>
2018-10-12 16:44:53 +0200

    pytorch cnn ok on CIFAR10

--- src/classifier/classifier_image_CNN_pytorch.py
+++ src/classifier/classifier_image_CNN_pytorch.py
@@ -35,6 +35,7 @@ from PIL import ImageOps
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import torchvision
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
@@ -46,8 +47,9 @@ IMG_SIZE = 32
 
 
 def imshow(img):
+    img = img / 2.0 + 0.5     # unnormalize
     npimg = img.numpy()
-    plt.imshow( npimg )
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
 
@@ -55,23 +57,28 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d( 1,1,5)
+        self.conv1 = nn.Conv2d( 3, 6, 5)
         self.pool = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d( 6, 16, 5)
         self.fc1 = nn.Linear( 16*5*5, 128)
         self.fc2 = nn.Linear(128, 84)
-        self.fc3 = nn.Linear(84, 5)         # 5 classes
+        self.fc3 = nn.Linear(84, 10)         # 5 classes
 
     def forward(self, x):
-        x = self.pool( F.relu(self.conv1(x)))
-        x = self.pool( F.relu(self.conv2(x)))
-        x = x.view( -1, 16*5*5)
+        x = self.pool( F.relu(self.conv1(x)))       # (c,w,h)  1x32x32 => 29*29*10 => 15*15*10
+        x = self.pool( F.relu(self.conv2(x)))       # 
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 
 
@@ -84,17 +91,46 @@ class Net(nn.Module):
 
 
 if __name__ == "__main__":
+
+
     TRANSFORM_IMG = transforms.Compose([
+        transforms.Grayscale(1),
         transforms.Resize(IMG_SIZE),
-        transforms.CenterCrop(64),
+        #transforms.CenterCrop(64),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5],
                                 std=[0.4, 0.4, 0.4] )
         ])
     mydata = ImageFolder(root="../../data/shapes5", transform=TRANSFORM_IMG)
-    loader = DataLoader(mydata, batch_size=32, shuffle=True, num_workers=2)
+    loader = DataLoader(mydata, batch_size=16, shuffle=True, num_workers=2)
+
+
+
+
+    transform = transforms.Compose(
+                [transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    mydata = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    loader = torch.utils.data.DataLoader(mydata, batch_size=4,
+                                            shuffle=True, num_workers=2)
+
+    testdata = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testdata, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
 
     print(mydata)
+    # get some random training images
+    dataiter = iter(loader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    #print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
     ############# NETWORK definition/configuration
@@ -110,6 +146,8 @@ if __name__ == "__main__":
         running_loss = 0.0
         for i,data in enumerate(loader,0):         # mini batch         
             inputs, labels = data
+            #print( "inputs shape=" + str(inputs.shape))
+            #print( "outputs shape=" + str(labels.shape))
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -131,12 +169,15 @@ if __name__ == "__main__":
     print('Finished Training')
     
     
-    ############ EVALUATION			  
+    ############ TEST
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % labels[j] for j in range(4)))
+
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join('%5s' % predicted[j] for j in range(4)))		  
     
 
-    ############ ONE SINGLE PREDICTION    
-    inputs, labels = next_batch(1)
-    inputs = torch.from_numpy(inputs)
-    labels = torch.from_numpy(labels).long()
-    outputs = net(inputs)
-    print(inputs[0], "is classified as ", outputs[0], " and real result is ", labels[0])
